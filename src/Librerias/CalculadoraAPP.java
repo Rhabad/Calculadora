c2acd37b72e12341349e54690ffedfaf9364440a
Rhabad <nicolaspizarro.450@gmail.com>
2023-08-11 10:07:11 -0400

    probando git

---
+++ src/Librerias/CalculadoraAPP.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Librerias;
+
+import Vistas.Calculadora;
+
+/**
+ *
+ * @author NICOLAS
+ */
+public class CalculadoraAPP {
+    public static void main(String[] args) {
+        Calculadora cal = new Calculadora();
+        
+        cal.setVisible(true);
+        cal.setResizable(false);
+        cal.setLocationRelativeTo(null);
+    }
+}
